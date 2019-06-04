60b118ae27c718a6d2e2608a81e8ad268c94b16f
Davey Newhall <newhall@bitwise.io>
2019-06-04 11:16:48 -0500

    WIP: update addressing for records
    Signed-off-by: Davey Newhall <newhall@bitwise.io>

--- track_and_trace/asset_client/src/utils/addressing.js
+++ track_and_trace/asset_client/src/utils/addressing.js
@@ -1,8 +1,11 @@
 const crypto = require("crypto")
 
 const TNT_FAMILY_NAME = 'grid_track_and_trace'
-const TNT_FAMILY_VERSION = '1.0'
 const TNT_NAMESPACE = 'a43b46'
+const TNT_RECORD_SUBSPACE = 'ec'
+const TNT_PROPOSAL_SUBSPACE = 'aa'
+const TNT_PROPERTY_SUBSPACE = 'ea'
+const TNT_FAMILY_VERSION = '1.0'
 
 const PIKE_FAMILY_NAME = 'pike'
 const PIKE_FAMILY_VERSION = '0.1'
@@ -10,20 +13,51 @@ const PIKE_NAMESPACE = 'cad11d'
 const PIKE_AGENT_SUBSPACE = '00'
 const PIKE_ORG_SUBSPACE = '01'
 
+const GRID_FAMILY_NAME = 'grid_schema'
+const GRID_FAMILY_VERSION = '1.0'
+const GRID_NAMESPACE = '621dee'
+const GRID_SCHEMA_SUBSPACE = '01'
+
 function hash(object, num) {
     let sha = crypto.createHash("sha512")
     return sha.update(object).digest("hex").substring(0, num)
 }
 
+function encodePage(page) {
+    return page.toString(16).padStart(4, '0')
+}
+
 module.exports = {
-    getTntFamilyNamespacePrefix() {
-        return TNT_NAMESPACE
+<<<<<<< HEAD
+=======
+    computeContractRegistryAddresses(families) {
+        return families.map((family) => SABRE_CONTRACT_REGISTRY_PREFIX + hash(family.name, 64))
     },
+    computeContractAddresses(families) {
+        return families.map((family) => SABRE_CONTRACT_PREFIX + hash(family.name + "," + family.version, 64))
+    },
+    computeNamespaceRegistryAddress(namespace) {
+        let prefix = namespace.substring(0, 6)
+        return SABRE_NAMESPACE_REGISTRY_PREFIX + hash(prefix, 64)
+    },
+>>>>>>> 869a88a1... fixup addressing
     makeAgentAddress(agentPublicKey) {
         return PIKE_NAMESPACE + PIKE_AGENT_SUBSPACE + hash(agentPublicKey, 62)
     },
     makeOrganizationAddress(organizationId) {
         return PIKE_NAMESPACE + PIKE_ORG_SUBSPACE + hash(organizationId, 62)
+    },
+    makeSchemaAddress(schemaName) {
+        return GRID_NAMESPACE + GRID_SCHEMA_SUBSPACE + hash(schemaName, 62)
+    },
+    makeRecordAddress(recordId) {
+        return TNT_NAMESPACE + TNT_RECORD_SUBSPACE + hash(recordId, 62)
+    },
+    makePropertyAddresses(recordId, propertyNames) {
+        return propertyNames.map((name) => TNT_NAMESPACE + TNT_PROPERTY_SUBSPACE + hash(recordId, 36) + hash(name, 22) + '0000')
+    },
+    makePropertyPageAddresses(recordId, propertyNames, currentPage) {
+        return propertyNames.map((name) => TNT_NAMESPACE + TNT_PROPERTY_SUBSPACE + hash(recordId, 36) + hash(name, 22) + encodePage(currentPage))
     },
     pikeFamily: {
         name: PIKE_FAMILY_NAME,
@@ -35,6 +69,20 @@ module.exports = {
         version: TNT_FAMILY_VERSION,
         namespace: TNT_NAMESPACE
     },
+    gridFamily: {
+        name: GRID_FAMILY_NAME,
+        version: GRID_FAMILY_VERSION,
+        namespace: GRID_NAMESPACE
+    },
+<<<<<<< HEAD
+=======
+    sabreFamily: {
+        name: SABRE_FAMILY_NAME,
+        version: SABRE_FAMILY_VERSION,
+    },
+    propertyAddressPrefix: TNT_NAMESPACE + TNT_PROPERTY_SUBSPACE,
+>>>>>>> 869a88a1... fixup addressing
     agentAddressPrefix: PIKE_NAMESPACE + PIKE_AGENT_SUBSPACE,
     organizationAddressPrefix: PIKE_NAMESPACE + PIKE_ORG_SUBSPACE,
+    schemaAddressPrefix: GRID_NAMESPACE + GRID_SCHEMA_SUBSPACE,
 }
